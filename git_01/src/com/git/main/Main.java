3a4fd74a69615a809ced3c81a927fb3eff5cdb51
ws <rntpwn5656@naver.com>
2021-02-03 17:09:20 +0900

    hotfix2 : Main

--- git_01/src/com/git/main/Main.java
+++ git_01/src/com/git/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		System.out.println("Hello JSP!");
 		System.out.println("Hello JSP!");
 		System.out.println("Hello Spring");
+		System.out.println("Hello jQuery!");
 	}
 
 }
